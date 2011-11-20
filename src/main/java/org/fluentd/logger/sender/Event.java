fd5b665548feb4c8fd7ef5886269cb201b100235
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-11-21 05:56:56 +0900

    changed package names of Event and EventTemplate classes

--- src/main/java/org/fluentd/logger/sender/Event.java
+++ src/main/java/org/fluentd/logger/sender/Event.java
@@ -15,7 +15,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 //
-package org.fluentd.logger;
+package org.fluentd.logger.sender;
 
 import java.util.Map;
 
--- src/main/java/org/fluentd/logger/sender/EventTemplate.java
+++ src/main/java/org/fluentd/logger/sender/EventTemplate.java
@@ -15,7 +15,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 //
-package org.fluentd.logger;
+package org.fluentd.logger.sender;
 
 import java.io.IOException;
 import java.util.HashMap;
