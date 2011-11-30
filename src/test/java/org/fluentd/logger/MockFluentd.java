3cf636e9a9f850addd4e2d6120a734b905b9340a
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-11-30 17:50:57 +0900

    added mock EventTemplate class for unit testing

--- src/test/java/org/fluentd/logger/MockFluentd.java
+++ src/test/java/org/fluentd/logger/MockFluentd.java
@@ -3,15 +3,50 @@ package org.fluentd.logger;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.fluentd.logger.sender.Event;
 import org.fluentd.logger.sender.EventTemplate;
 import org.msgpack.MessagePack;
+import org.msgpack.MessageTypeException;
+import org.msgpack.packer.Packer;
+import org.msgpack.template.Templates;
+import org.msgpack.unpacker.Unpacker;
 
 public class MockFluentd extends Thread {
 
     public static interface MockProcess {
         public void process(MessagePack msgpack, Socket socket) throws IOException;
+    }
+
+    public static class MockEventTemplate extends EventTemplate {
+        public static MockEventTemplate INSTANCE = new MockEventTemplate();
+
+        public Event read(Unpacker u, Event to, boolean required) throws IOException {
+            if (!required && u.trySkipNil()) {
+                return null;
+            }
+
+            to = new Event();
+            u.readArrayBegin();
+            {
+                to.tag = Templates.TString.read(u, null, required);
+                to.timestamp = Templates.TLong.read(u, null, required);
+                int size = u.readMapBegin();
+                to.data = new HashMap<String, Object>(size);
+                {
+                    for (int i = 0; i < size; i++) {
+                        String key = Templates.TString.read(u, null, required);
+                        String value = Templates.TString.read(u, null, required);
+                        to.data.put(key, value);
+                    }
+                }
+                u.readMapEnd();
+            }
+            u.readArrayEnd();
+            return to;
+        }
     }
 
     private MessagePack msgpack;
@@ -22,7 +57,7 @@ public class MockFluentd extends Thread {
 
     public MockFluentd(int port, MockProcess mockProcess) throws IOException {
         msgpack = new MessagePack();
-        msgpack.register(Event.class, EventTemplate.INSTANCE);
+        msgpack.register(Event.class, MockEventTemplate.INSTANCE);
         serverSocket = new ServerSocket(port);
         process = mockProcess;
     }
