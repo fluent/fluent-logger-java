95ed1f9fd1e0b576b169c799fb603fa940d93a4a
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-05-11 16:21:18 -0700

    refactored EventTemplate class

--- src/main/java/org/fluentd/logger/sender/Event.java
+++ src/main/java/org/fluentd/logger/sender/Event.java
@@ -17,7 +17,14 @@
 //
 package org.fluentd.logger.sender;
 
+import java.io.IOException;
 import java.util.Map;
+
+import org.msgpack.MessageTypeException;
+import org.msgpack.packer.Packer;
+import org.msgpack.template.AbstractTemplate;
+import org.msgpack.template.Templates;
+import org.msgpack.unpacker.Unpacker;
 
 public class Event {
     public String tag;
@@ -39,5 +46,43 @@ public class Event {
     public String toString() {
         return String.format("Event { tag=%s, timestamp=%d, data=%s }",
                 new Object[] { tag, timestamp, data.toString() });
+    }
+
+    public static class EventTemplate extends AbstractTemplate<Event> {
+        public static EventTemplate INSTANCE = new EventTemplate();
+
+        public void write(Packer pk, Event v, boolean required) throws IOException {
+            if (v == null) {
+                if (required) {
+                    throw new MessageTypeException("Attempted to write null");
+                }
+                pk.writeNil();
+                return;
+            }
+
+            pk.writeArrayBegin(3);
+            {
+                Templates.TString.write(pk, v.tag, required);
+                Templates.TLong.write(pk, v.timestamp, required);
+                pk.writeMapBegin(v.data.size());
+                {
+                    for (Map.Entry<String, Object> entry : v.data.entrySet()) {
+                        Templates.TString.write(pk, entry.getKey(), required);
+                        try {
+                            pk.write(entry.getValue());
+                        } catch (MessageTypeException e) {
+                            String val = entry.getValue().toString();
+                            Templates.TString.write(pk, val, required);
+                        }
+                    }
+                }
+                pk.writeMapEnd();
+            }
+            pk.writeArrayEnd();
+        }
+
+        public Event read(Unpacker u, Event to, boolean required) throws IOException {
+            throw new UnsupportedOperationException("Don't need the operation");
+        }
     }
 }
